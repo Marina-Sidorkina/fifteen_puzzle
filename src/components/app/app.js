ab44c1814426dbf4de6e8c711201cef8005f510e
Marina Sidorkina <m.sidorkina55@gmail.com>
2020-12-05 22:21:20 +0300

    Add componentDidUpdate basic variant.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -12,16 +12,24 @@ export default class App extends Component {
         {x: 0, y: 2}, {x: 1, y: 2}, {x: 2, y: 2}, {x: 3, y: 2},
         {x: 0, y: 3}, {x: 1, y: 3}, {x: 2, y: 3}, {x: 3, y: 3}
       ],
-      tiles: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+      tiles: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+      winner: false
     };
 
     this.onTileClick = this.onTileClick.bind(this);
   }
 
+  componentDidUpdate() {
+    const check = this.state.tiles.every((tile, index) => tile === (index + 1));
+    if(check && !this.state.winner) {
+      this.setState({winner: true});
+      console.log("Winner");
+    };
+  }
+
   replaceTiles(emptyTileId, movedTileId, movedTileValue) {
     this.setState(({ tiles }) => {
       const newArray = tiles.slice();
-
       newArray.splice(emptyTileId, 1, movedTileValue);
       newArray.splice(movedTileId, 1, 16);
 
